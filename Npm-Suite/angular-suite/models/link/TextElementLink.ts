76188032ed7b2a3be6f346f95c1ec4941a113531
theoliverlear <sigwartholiver2@gmail.com>
2025-08-24 14:05:45 -0500

    Fix TextElementLink.ts bug where it duplicated `ElementLink` model

--- Npm-Suite/angular-suite/models/link/TextElementLink.ts
+++ Npm-Suite/angular-suite/models/link/TextElementLink.ts
@@ -1,14 +1,21 @@
-import {TargetType} from "../html/TargetType";
+import { ElementLink } from "./ElementLink";
+import { TargetType } from "../html/TargetType";
+import {TagType} from "../html/TagType";
 
-export class ElementLink {
-    hrefLink: string;
-    targetType: TargetType;
-    useHyperlinkStyle: boolean;
+export class TextElementLink extends ElementLink {
+    text: string;
+    tagType: TagType;
     constructor(hrefLink: string = '',
                 targetType: TargetType = TargetType.SELF,
-                useHyperlinkStyle: boolean = false) {
-        this.hrefLink = hrefLink;
-        this.targetType = targetType
-        this.useHyperlinkStyle = useHyperlinkStyle;
+                useHyperlinkStyle: boolean = false,
+                text: string = '',
+                tagType: TagType = TagType.P) {
+        super(hrefLink, targetType, useHyperlinkStyle);
+        this.text = text;
+        this.tagType = tagType;
+    }
+    
+    hasText(): boolean {
+        return this.text.length > 0;
     }
 }
