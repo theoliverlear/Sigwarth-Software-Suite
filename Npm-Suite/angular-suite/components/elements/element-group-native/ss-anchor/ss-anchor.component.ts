a656a437dc79d26878752222ab527eec2acc6fed
theoliverlear <sigwartholiver2@gmail.com>
2025-08-24 14:18:46 -0500

    Mark `SsAnchorComponent` as standalone and add `CommonModule` import for improved modularity in ss-anchor.component.ts

--- Npm-Suite/angular-suite/components/elements/element-group-native/ss-anchor/ss-anchor.component.ts
+++ Npm-Suite/angular-suite/components/elements/element-group-native/ss-anchor/ss-anchor.component.ts
@@ -4,17 +4,24 @@ import {
 } from "@angular/core";
 import {TagType} from "../../../../models/html/TagType";
 import {Router} from "@angular/router";
+import {CommonModule} from "@angular/common";
 import {ElementLink} from "../../../../models/link/ElementLink";
 import {TextElementLink} from "../../../../models/link/TextElementLink";
 
 @Component({
     selector: 'ss-anchor',
+    standalone: true,
+    imports: [
+        CommonModule
+    ],
     templateUrl: './ss-anchor.component.html',
     styleUrls: ['./ss-anchor.component.scss']
 })
 export class SsAnchorComponent implements OnInit {
     @Input() elementLink: TextElementLink | ElementLink;
     hasText: boolean = false;
+    displayText: string = '';
+    displayTagType: TagType = TagType.SPAN;
     @Input() isButton: boolean = false;
     @HostBinding('class.link-button') get isButtonClass(): boolean {
         return this.isButton;
@@ -25,6 +32,13 @@ export class SsAnchorComponent implements OnInit {
     }
     ngOnInit() {
         this.hasText = this.elementLink instanceof TextElementLink;
+        if (this.elementLink instanceof TextElementLink) {
+            this.displayText = this.elementLink.text;
+            this.displayTagType = (this.elementLink as TextElementLink).tagType ?? TagType.SPAN;
+        } else {
+            this.displayText = '';
+            this.displayTagType = TagType.SPAN;
+        }
     }
     @HostListener('click', ['$event'])
     onClick() {
