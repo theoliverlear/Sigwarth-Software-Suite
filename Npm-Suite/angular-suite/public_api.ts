c0ccc826e3f3a352bfda03d7760a85f78646a6e9
theoliverlear <sigwartholiver2@gmail.com>
2025-08-30 14:05:27 -0500

    Expand public_api.ts to export additional models and components for increased functionality in `angular-suite`

--- Npm-Suite/angular-suite/public_api.ts
+++ Npm-Suite/angular-suite/public_api.ts
@@ -6,12 +6,16 @@ export * from './modules/angular-suite.module';
 export * from './components/elements/elements';
 export * from './components/elements/element-group-native/ss-anchor/ss-anchor.component';
 export * from './components/elements/element-group-native/ss-button/ss-button.component';
+export * from './components/elements/element-group-native/ss-button/models/ButtonPosition';
+export * from './components/elements/element-group-native/ss-button/models/ButtonText';
 export * from './components/elements/element-group-native/ss-footer/ss-footer.component';
 export * from './components/elements/element-group-native/ss-head/ss-head.component';
 export * from './components/elements/element-group-native/ss-img/ss-img.component';
 export * from './components/elements/element-group-native/ss-input/ss-input.component';
+export * from './components/elements/element-group-native/ss-input/models/InputType';
 export * from './components/elements/element-group-native/ss-paragraph/ss-paragraph.component';
 export * from './components/elements/element-group-native/ss-title/ss-title.component';
+export * from './components/elements/element-group-text/subtitle/subtitle.component';
 
 // Directives
 export * from './directives/directives';
@@ -30,6 +34,7 @@ export * from './services/net/http/httpProperties';
 export * from './services/net/websocket/websocket.service';
 
 // Models
+export * from './models/ElementSize';
 export * from './models/auth/AuthType';
 export * from './models/auth/AuthPopup';
 export * from './models/html/TagType';
@@ -39,3 +44,4 @@ export * from './models/link/TextElementLink';
 
 // Routing helpers (types only, in case consumers want them)
 export * from './modules/routing/routes';
+export * from './modules/routing/app-routing.module';
