6ec0bce42a4a407cb0bd527b155e38cdfebd2e0a
theoliverlear <sigwartholiver2@gmail.com>
2025-09-08 14:52:08 -0500

    Expand angular-suite.module.ts to declare, export new components and directives, and replace deprecated imports

--- Npm-Suite/angular-suite/modules/angular-suite.module.ts
+++ Npm-Suite/angular-suite/modules/angular-suite.module.ts
@@ -1,29 +1,67 @@
 import {NgModule} from "@angular/core";
-import {elements} from "../components/elements/elements";
-import {directives} from "../directives/directives";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {provideHttpClient, withFetch} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {services} from "../services/services";
+import {RouterModule} from "@angular/router";
+
+// Components
+import {SsAnchorComponent} from "../components/elements/element-group-native/ss-anchor/ss-anchor.component";
+import {SsButtonComponent} from "../components/elements/element-group-native/ss-button/ss-button.component";
+import {SsFooterComponent} from "../components/elements/element-group-native/ss-footer/ss-footer.component";
+import {SsHeadComponent} from "../components/elements/element-group-native/ss-head/ss-head.component";
+import {SsImgComponent} from "../components/elements/element-group-native/ss-img/ss-img.component";
+import {SsInputComponent} from "../components/elements/element-group-native/ss-input/ss-input.component";
+import {SsParagraphComponent} from "../components/elements/element-group-native/ss-paragraph/ss-paragraph.component";
+import {SsTitleComponent} from "../components/elements/element-group-native/ss-title/ss-title.component";
+import {SubtitleComponent} from "../components/elements/element-group-text/subtitle/subtitle.component";
+
+// Directives
+import {UndraggableDirective} from "../directives/undraggable.directive";
+import {UnoptimizedImageDirective} from "../directives/unoptimized-image.directive";
 
 @NgModule({
     declarations: [
+        // components
+        SsAnchorComponent,
+        SsButtonComponent,
+        SsFooterComponent,
+        SsHeadComponent,
+        SsImgComponent,
+        SsInputComponent,
+        SsParagraphComponent,
+        SsTitleComponent,
+        SubtitleComponent,
+        // directives
+        UndraggableDirective,
+        UnoptimizedImageDirective,
     ],
     imports: [
         CommonModule,
         FormsModule,
         NgOptimizedImage,
-        ...elements,
-        ...directives,
+        HttpClientModule,
+        RouterModule,
     ],
     providers: [
         ...services,
-        provideHttpClient(withFetch()),
     ],
     exports: [
-        ...elements,
-        ...directives,
-        NgOptimizedImage,
+        // components
+        SsAnchorComponent,
+        SsButtonComponent,
+        SsFooterComponent,
+        SsHeadComponent,
+        SsImgComponent,
+        SsInputComponent,
+        SsParagraphComponent,
+        SsTitleComponent,
+        SubtitleComponent,
+        // directives
+        UndraggableDirective,
+        UnoptimizedImageDirective,
+        // modules for consumers
+        RouterModule
     ],
 })
 export class AngularSuiteModule {}
