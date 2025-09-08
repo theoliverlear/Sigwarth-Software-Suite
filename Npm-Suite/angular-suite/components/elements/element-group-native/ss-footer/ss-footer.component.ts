9d09e286e6c7cfb7cb5daf4c7c7c36afb286de5e
theoliverlear <sigwartholiver2@gmail.com>
2025-09-08 07:30:55 -0500

    Expand ss-footer.component.ts to enable standalone functionality and include `CommonModule` import

--- Npm-Suite/angular-suite/components/elements/element-group-native/ss-footer/ss-footer.component.ts
+++ Npm-Suite/angular-suite/components/elements/element-group-native/ss-footer/ss-footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ss-footer',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './ss-footer.component.html',
   styleUrls: ['./ss-footer.component.scss']
 })
 export class SsFooterComponent {
-
+    constructor() {
+        
+    }
 }
