c3a25060839e77f7461df17b231b720f9f7b7cac
theoliverlear <sigwartholiver2@gmail.com>
2025-09-08 10:32:25 -0500

    Refactor angular-suite.module.ts to remove unused imports and dependencies

--- Npm-Suite/angular-suite/modules/angular-suite.module.ts
+++ Npm-Suite/angular-suite/modules/angular-suite.module.ts
@@ -1,27 +1,18 @@
 import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
 import {elements} from "../components/elements/elements";
 import {directives} from "../directives/directives";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
+import {provideHttpClient, withFetch} from "@angular/common/http";
 import {services} from "../services/services";
-import {AppRouting} from "./routing/app-routing.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {RouterModule} from "@angular/router";
 
 @NgModule({
     declarations: [
     ],
     imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
         CommonModule,
         FormsModule,
         NgOptimizedImage,
-        HttpClientModule,
-        RouterModule,
-        AppRouting,
         ...elements,
         ...directives,
     ],
@@ -33,8 +24,6 @@ import {RouterModule} from "@angular/router";
         ...elements,
         ...directives,
         NgOptimizedImage,
-        RouterModule,
-        AppRouting
     ],
 })
 export class AngularSuiteModule {}
