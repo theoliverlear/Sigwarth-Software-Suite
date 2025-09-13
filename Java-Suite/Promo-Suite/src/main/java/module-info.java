7717e9fe8328bbb85673d3216220b953e5d10118
theoliverlear <sigwartholiver2@gmail.com>
2025-09-13 13:10:30 -0500

    Update module-info.java to declare `annotations` module as static dependency

--- Java-Suite/Promo-Suite/src/main/java/module-info.java
+++ Java-Suite/Promo-Suite/src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.sigwarthsoftware.promo {
     requires com.sigwarthsoftware.openai;
     requires org.apache.httpcomponents.httpclient;
     requires org.apache.httpcomponents.httpcore;
-    requires annotations;
+    requires static annotations;
     requires com.fasterxml.jackson.databind;
     requires static lombok;
     requires org.slf4j;
