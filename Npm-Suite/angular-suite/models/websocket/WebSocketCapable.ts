03d7f71ae8dc2b70a80d2920a1ce7a50e1c625b4
theoliverlear <sigwartholiver2@gmail.com>
2025-09-13 10:30:19 -0500

    Refactor WebSocketCapable.ts to rename WebSocket method for clarity and accuracy

--- Npm-Suite/angular-suite/models/websocket/WebSocketCapable.ts
+++ Npm-Suite/angular-suite/models/websocket/WebSocketCapable.ts
@@ -2,5 +2,5 @@ import {Subscription} from "rxjs";
 
 export interface WebSocketCapable {
     webSocketSubscriptions: Record<string, Subscription>;
-    initializeWebSocket(): void;
+    initializeWebSockets(): void;
 }
